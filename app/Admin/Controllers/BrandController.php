68431bb0376faadff2746ea07d6cda3314841034
fanzewei <fanzewei@yongche.com>
2021-10-22 10:26:39 +0800

    商品管理优化

--- app/Admin/Controllers/BrandController.php
+++ app/Admin/Controllers/BrandController.php
@@ -1,36 +1,43 @@
 <?php
 
-
 namespace App\Admin\Controllers;
 
-
 use App\Models\Brand;
-
 use SmallRuralDog\Admin\Components\Form\Upload;
 use SmallRuralDog\Admin\Components\Grid\Image;
 use SmallRuralDog\Admin\Controllers\AdminController;
-use SmallRuralDog\Admin\Controllers\AdminResource;
 use SmallRuralDog\Admin\Form;
 use SmallRuralDog\Admin\Grid;
 
 class BrandController extends AdminController
 {
-
     public function grid()
     {
         $grid = new Grid(new Brand());
 
-        $grid->quickSearch(['name']);
+        $grid->addDialogForm($this->form()->isDialog()->className('p-15'));
+        $grid->editDialogForm($this->form(true)->isDialog()->className('p-15'));
 
-        $grid->column("id", "ID")->width(50);
-        $grid->column("icon", "图标")->component(Image::make()->size(50, 50))->width(70)->align("center");
-        $grid->column("name", "品牌名称")->width(150);
-        $grid->column("index_name", "索引")->width(50)->align("center");
-        $grid->column("source", "产地国家")->width(100)->align("center");
-        $grid->column("source_icon", "索引")->component(Image::make()->size(30, 30))->width(50)->align("center");
+        $grid->pageBackground()
+            ->defaultSort('id', 'desc')
+            ->quickSearch(['name', 'index_name'])
+            ->stripe(true)
+            ->fit(true)
+            ->defaultSort('id', 'desc')
+            ->perPage(env('PER_PAGE', 15))
+            ->size(env('TABLE_SIZE', ''))
+            ->border(env('TABLE_BORDER', false))
+            ->emptyText("暂无数据");
 
-        $grid->toolbars(function (Grid\Toolbars $toolbars){
-            $toolbars->addRight(Grid\Tools\ToolButton::make("查看源代码")->handler("link")->uri("https://github.com/SmallRuralDog/laravel-vue-admin-demo/blob/master/app/Admin/Controllers/BrandController.php"));
+        $grid->column("id", "序号")->width(80)->align('center')->sortable();
+        $grid->column("name", "品牌名称")->width(200);
+        $grid->column("index_name", "索引首字母")->width(150)->align("center");
+        $grid->column("icon", "品牌logo")->component(Image::make()->size(50, 50))->width(100)->align("center");
+        $grid->column("source", "产地国家")->width(200)->align("center");
+        $grid->column("source_icon", "产地图标")->component(Image::make()->size(50, 50))->width(100)->align("center");
+
+        $grid->toolbars(function (Grid\Toolbars $toolbars) {
+            $toolbars->createButton()->content("添加品牌");
         });
 
         return $grid;
@@ -38,15 +45,14 @@ class BrandController extends AdminController
 
     public function form($isEdit = false)
     {
-
         $form = new Form(new Brand());
+        $form->getActions()->buttonCenter();
 
-        $form->item("name", "品牌名称")->required()->inputWidth(5);
-        $form->item("index_name", "索引首字母")->required()->inputWidth(5);
+        $form->item("name", "品牌名称")->required()->inputWidth(10);
+        $form->item("index_name", "索引首字母")->required()->inputWidth(10);
         $form->item("icon", "品牌logo")->required()->component(Upload::make()->width(80)->height(80));
-        $form->item("source", "产地国家")->required()->inputWidth(5);
+        $form->item("source", "产地国家")->required()->inputWidth(10);
         $form->item("source_icon", "产地图标")->required()->component(Upload::make()->width(80)->height(80));
-
 
         return $form;
     }
--- app/Admin/Controllers/ShopController.php
+++ app/Admin/Controllers/ShopController.php
@@ -1,37 +1,23 @@
 <?php
 
-
 namespace App\Admin\Controllers;
 
-
 use Andruby\DeepAdmin\Controllers\ContentController;
-use App\Models\Brand;
-use App\Models\Goods;
-use App\Models\GoodsAttr;
-use App\Models\GoodsAttrMap;
-use App\Models\GoodsAttrValue;
-use App\Models\GoodsAttrValueMap;
-use App\Models\GoodsClass;
-use Illuminate\Http\Request;
-use SmallRuralDog\Admin\Components\Attrs\SelectOption;
-use SmallRuralDog\Admin\Components\Form\Cascader;
-use SmallRuralDog\Admin\Components\Form\CSwitch;
-use SmallRuralDog\Admin\Components\Form\DatePicker;
-use SmallRuralDog\Admin\Components\Form\Input;
-use SmallRuralDog\Admin\Components\Form\Radio;
-use SmallRuralDog\Admin\Components\Form\RadioGroup;
-use SmallRuralDog\Admin\Components\Form\Select;
-use SmallRuralDog\Admin\Components\Form\Upload;
-use SmallRuralDog\Admin\Components\Form\WangEditor;
-use SmallRuralDog\Admin\Components\Grid\Image;
-use SmallRuralDog\Admin\Components\Grid\Tag;
-use SmallRuralDog\Admin\Components\Widgets\Divider;
-use SmallRuralDog\Admin\Controllers\AdminController;
-use SmallRuralDog\Admin\Form;
 use SmallRuralDog\Admin\Grid;
-use Andruby\DeepAdmin\Components\GoodsSku;
 
 class ShopController extends ContentController
 {
+    function grid_list(Grid $grid)
+    {
+        $grid->quickSearch(['name']);
 
+        $grid->addDialogForm($this->form()->isDialog()->className('p-15'));
+        $grid->editDialogForm($this->form(true)->isDialog()->className('p-15'));
+
+        $grid->toolbars(function (Grid\Toolbars $toolbars) {
+            $toolbars->createButton()->content("添加店铺");
+        });
+
+        return $grid;
+    }
 }
