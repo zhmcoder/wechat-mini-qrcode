c71e07859c230997230271171d508a4b018a1f9d
zhaomeng <zhmzhaomeng@hotmail.com>
2021-11-07 08:14:57 +0800

    接口测试

--- app/Api/routes.php
+++ app/Api/routes.php
@@ -3,11 +3,11 @@
 use Illuminate\Routing\Router;
 
 Route::group([
-    'prefix' => 'api',
+    'prefix' => 'Api',
     'namespace' => 'App\\Api\\Controllers',
     'middleware' => 'verifySign'
 ], function (Router $router) {
     $router->post('user/login', 'UserController@userLogin')->name('user.login');
 
-    $router->get('home/index', 'HomeController@index')->name('home.index');
+    $router->post('Home/index', 'HomeController@index')->name('home.index');
 });
---
+++ config/deep_mall.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'api_secret'=>[
+        'com.deep.mall'=>'121Dkudisa*82iiouS9IDw*'
+    ]
+];
